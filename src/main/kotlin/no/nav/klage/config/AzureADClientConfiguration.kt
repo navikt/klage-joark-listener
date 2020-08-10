996466aa7daeafcd9755efbe76b43cfe3b1334b5
Richard Borge <richard.borge@nav.no>
2020-08-10 15:12:16 +0200

    Proxy-config

--- src/main/kotlin/no/nav/klage/config/AzureADClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/AzureADClientConfiguration.kt
@@ -2,9 +2,14 @@ package no.nav.klage.config
 
 import no.nav.klage.client.OidcDiscoveryClient
 import no.nav.klage.util.getLogger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.ProxyProvider
+
 
 @Configuration
 class AzureADClientConfiguration(
@@ -17,10 +22,21 @@ class AzureADClientConfiguration(
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
+    @Value("\${HTTP_PROXY}")
+    private lateinit var proxyUrl: String
+
     @Bean
     fun azureADWebClient(): WebClient {
+        val httpClient: HttpClient = HttpClient.create()
+            .tcpConfiguration { tcpClient ->
+                tcpClient.proxy { proxy ->
+                    proxy.type(ProxyProvider.Proxy.HTTP).host(proxyUrl)
+                }
+            }
+        val connector = ReactorClientHttpConnector(httpClient)
         return webClientBuilder
                 .baseUrl(oidcDiscoveryClient.oidcDiscovery().token_endpoint)
+                .clientConnector(connector)
                 .build()
     }
 }
