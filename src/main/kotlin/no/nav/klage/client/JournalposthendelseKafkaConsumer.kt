253d8b8be0015a8feabaab75ee6db305f6300109
Richard Borge <richard.borge@nav.no>
2020-08-17 09:53:00 +0200

    Generating xml and logging

--- src/main/kotlin/no/nav/klage/client/JournalposthendelseKafkaConsumer.kt
+++ src/main/kotlin/no/nav/klage/client/JournalposthendelseKafkaConsumer.kt
@@ -3,9 +3,13 @@ package no.nav.klage.client
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.klage.util.getLogger
 import no.nav.klage.varsel.VarselSender
+import no.nav.klage.varsel.varselFromJournalfoeringHendelse
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+
+private const val behandlingstemaKlageUnderenhet = "ab0019"
 
 @Component
 class JournalposthendelseKafkaConsumer(
@@ -18,15 +22,36 @@ class JournalposthendelseKafkaConsumer(
         private val logger = getLogger(javaClass.enclosingClass)
     }
 
+    @Value("\${NAIS_CLUSTER_NAME}")
+    private lateinit var cluster: String
+
     @KafkaListener(topics = ["\${KAFKA_TOPIC}"])
     fun listen(journalpostRecord: ConsumerRecord<String, JournalfoeringHendelseRecord>) {
         logger.debug("Journalposthendelse received from Kafka topic: {}", journalpostRecord)
 
-        //TODO only fetch the ones we care about
-//        logger.debug("Fetching journalpost from SAF")
-//        val journalpostResponse = safClient.getJournalpost(journalpostRecord.value().journalpostId.toString())
-//        logger.debug("Journalpost fetched from SAF: {}", journalpostResponse)
-
-        // TODO Send varsel
+        val record = journalpostRecord.value()
+        if (record.sendVarsel()) {
+            val journalpostResponse = safClient.getJournalpost(record.journalpostId.toString())
+            journalpostResponse.data?.journalpost?.run {
+                runCatching {
+                    varselSender.send(
+                        record.hendelsesId.toString(), varselFromJournalfoeringHendelse(
+                            aktoerId = bruker?.id ?: throw Exception("Aktoerid not present in record"),
+                            fornavn = avsenderMottakerNavn ?: throw Exception("User name not present in record"),
+                            journalfoeringsdato = datoOpprettet ?: throw Exception("Date not present in record")
+                        )
+                    )
+                }.onFailure {
+                    logger.error("Sending varsel for ${record.hendelsesId} failed.", it)
+                }.onSuccess {
+                    logger.debug("Varsel sent for ${record.hendelsesId}")
+                }
+            }
+        }
     }
+
+    private fun JournalfoeringHendelseRecord.sendVarsel(): Boolean =
+        this.behandlingstema.toString() == behandlingstemaKlageUnderenhet &&
+                this.journalpostStatus.toString() == "M" &&
+                cluster == "dev-fss" // TODO Onle send varsel in dev until verified
 }
